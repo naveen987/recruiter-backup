50b8d8eddca851d7f88d400de23dac344b46c843
naveen987 <tsnaveentumkur@gmail.com>
2023-09-16 18:26:55 +0200

    chnages done

--- src/evaluation_system/evaluate.py
+++ src/evaluation_system/evaluate.py
@@ -1,36 +1,193 @@
+#importing required packages/lib
+
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
+from matplotlib import pyplot as plt
 
-def get_best_cluster_users():
-    # Load the dataset
-    data_path = 'stack.csv'
-    data = pd.read_csv(data_path)
+#importing data from csv to pandas df
+data = pd.read_csv("Stack.csv")
+data
 
-    # Convert the 'Reputation' column to numeric format
-    data['Reputation'] = data['Reputation'].str.replace(',', '').astype(int)
+data.columns
+data.dtypes
 
-    # Extract the columns related to job profiles
-    job_profile_columns = data.columns[11:-1]
+ #checking for null/nan values
+nan_values = data.isna().sum()
+nan_values[nan_values > 0]
 
-    # Create a subset with relevant columns for clustering
-    data_subset = data[['Membership Period', 'Reputation'] + list(job_profile_columns)]
-    data_subset = data_subset.drop(columns=['Number of Tags', 'User Tags'])
 
-    # Scale the data
-    scaler = StandardScaler()
-    data_scaled = scaler.fit_transform(data_subset)
+#cleaning badges data points and creating new feature as 'total_badges'
+data['Bronze Badges'] = data['Bronze Badges'].str.replace(',', '').astype(float)
+data['Bronze Badges'] = data['Bronze Badges'].fillna(0)
+bronze = data['Bronze Badges']
+golden = data['Gold Badges']
+silver = data['Silver Badges'].str.replace(',', '').astype(float)
+data['total_badges'] = golden + silver + bronze
 
-    # K-means clustering
-    optimal_clusters = 4
-    kmeans = KMeans(n_clusters=optimal_clusters, init='k-means++', max_iter=300, n_init=10, random_state=0)
-    data['cluster_group'] = kmeans.fit_predict(data_scaled)
 
-    # Identify the cluster with the highest average reputation
-    best_cluster = data.groupby('cluster_group')['Reputation'].mean().idxmax()
+#converting 'reputation' and 'reached' columns to numeric
+data['reputation'] = data['Reputation'].str.replace(',', '').astype(float)
+def clean_reached(value):
+    if 'k' in value:
+        return float(value.replace('k', '')) * 1000
+    elif 'm' in value:
+        return float(value.replace('m', '')) * 1000000
+    else:
+        return float(value)
+data['reached'] = data['Reached'].apply(clean_reached)
+data['membership_period'] = data['Membership Period']
 
-    # Extract users from the best cluster and sort them by reputation in descending order
-    best_cluster_users = data[data['cluster_group'] == best_cluster][['sequential_id', 'Name', 'Reputation']]
-    best_cluster_users = best_cluster_users.sort_values(by='Reputation', ascending=False)
+#creating new feature as 'total_skills'
+skill_columns = ['Python Developer','C Developer', 'C++ Developer', 'Java Developer', 'C# Developer','JavaScript Developer', 'SQL Developer', 'R Developer', 'Data Engineer','Data Scientist', 'Artificial Intelligence Specialist','Machine Learning Expert']
+data['total_skills'] = data[skill_columns].sum(axis=1)
+
+#creating new feature as 'skills_ratio'
+data['skills_ratio'] = data['total_skills'] / data['membership_period']
+
+max_skills_ratio = data[data['skills_ratio'] != float('inf')]['skills_ratio'].max()
+data['skills_ratio'].replace(float('inf'), max_skills_ratio, inplace=True)
+
+
+#droping unecessary columns
+data = data.drop(['Membership Period', 'Job Title', 'Reputation',
+       'Reached', 'Answers', 'Questions', 'Gold Badges', 'Silver Badges',
+       'Bronze Badges', 'Number of Tags', 'User Tags', 'Python Developer',
+       'C Developer', 'C++ Developer', 'Java Developer', 'C# Developer',
+       'JavaScript Developer', 'SQL Developer', 'R Developer', 'Data Engineer',
+       'Data Scientist', 'Artificial Intelligence Specialist',
+       'Machine Learning Expert'],axis=1)
+#checking for null/nan values
+nan_values = data.isnull().sum()
+nan_values[nan_values > 0]
+
+data.columns
+
+#standardizing the data
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+columns_to_scale = ['total_badges', 'reputation', 'reached']
+df = data[columns_to_scale]
+scaled_data = scaler.fit_transform(df)
+
+data[columns_to_scale] = scaled_data
+
+#creating new feature as 'badge_to_reputation_ratio'
+data['badge_to_reputation_ratio'] = data['total_badges'] / data['reputation']
+
+#creating new feature as 'badge_per_membership_period'
+data['badge_per_membership_period'] = data['total_badges']/data['membership_period']
+
+data
+
+#creating a subset of data to perform clustering algorithm
+columns_to_cluster = ['total_badges', 'reputation', 'reached']
+cluster_dataset = data[columns_to_cluster]
+cluster_dataset
+
+#finding the ideak cluster value k using elbow method
+sse = []
+k_rng = range(1,15)
+for k in k_rng:
+    K_means = KMeans(n_clusters=k)
+    K_means.fit_predict(cluster_dataset)
+    sse.append(K_means.inertia_)
+plt.xlabel('K')
+plt.ylabel('Sum of squared error')
+plt.plot(k_rng,sse)
+
+#the plot displays k ==5 (need to generalise this step)
+K_means = KMeans(n_clusters = 5)
+K_means
+data_p = K_means.fit_predict(cluster_dataset)
+data_p
+data['cluster'] = data_p
+data
+
+data.cluster.unique()
+
+#checking the number of records under each cluster
+cluster_counts = data['cluster'].value_counts()
+cluster_counts
+
+clusters_above_threshold = cluster_counts[cluster_counts > 100].index
+clusters_above_threshold
+
+#seleting the best cluster based on high mean values for 'reputation' and 'total_badges' features
+cluster_scores = {}
+for cluster in clusters_above_threshold:
+    cluster_data = data[data['cluster'] == cluster]
+    combined_score = cluster_data['reputation'].mean() + cluster_data['total_badges'].mean()
+    cluster_scores[cluster] = combined_score
+cluster_scores
+
+best_cluster = max(cluster_scores, key=cluster_scores.get)
+
+best_cluster, cluster_scores[best_cluster]
+
+
+best_cluster_data = data[data.cluster == best_cluster]
+best_cluster_data
+
+#clustering the best_cluster_data again based on different membership period
+membership_data = best_cluster_data['membership_period'].values.reshape(-1, 1)
+membership_data
+
+
+sse = []
+k_rng = range(1,10)
+for k in k_rng:
+    K_means_new_cluster = KMeans(n_clusters=k)
+    K_means_new_cluster.fit_predict(membership_data)
+    sse.append(K_means_new_cluster.inertia_)
+plt.xlabel('K')
+plt.ylabel('Sum of squared error')
+plt.plot(k_rng,sse)
+
+#the plot displays k ==3 (need to generalise this step, by def a function that takes a data farme performs sse plot and gets the ideal cluster value)
+K_means_new_cluster = KMeans(n_clusters=3)
+membership_clusters = K_means_new_cluster.fit_predict(membership_data)
+membership_clusters
+
+best_cluster_data['membership_clusters'] = membership_clusters
+
+best_cluster_statistics_data = best_cluster_data.drop(['Name', 'sequential_id'],axis=1)
+best_cluster_statistics = best_cluster_statistics_data.groupby('membership_clusters').mean()
+
+best_cluster_statistics.membership_period
+
+cluster_means = {}
+for index, value in enumerate(best_cluster_statistics.membership_period):
+    cluster_means[index] = value
     
-    return best_cluster_users
+#sorting the clusters based on these mean values
+sorted_clusters = sorted(cluster_means, key=cluster_means.get)
+
+#assigning labels in ascending order
+label_mapping = {cluster: i for i, cluster in enumerate(sorted_clusters)}
+label_mapping
+
+#labeling the clusters
+best_cluster_data['membership_clusters_ordered'] = best_cluster_data['membership_clusters'].map(label_mapping)
+
+best_cluster_data
+
+new_label_mapping = {
+    0: "New member",
+    1: "Mid-Tenure Members",
+    2: "Veteran Members"}
+
+#replacing the numeric labels with the desired string labels
+best_cluster_data['membership_clusters_ordered'] = best_cluster_data['membership_clusters_ordered'].map(new_label_mapping)
+best_cluster_data
+
+#exporting top 10 records from each label
+def get_best_cluster_users(df, column_name, count=10):
+    
+    top_records = df.groupby(column_name).apply(
+        lambda x: x.nlargest(count, ['reputation', 'total_badges'])
+    ).reset_index(drop=True)
+    
+    return top_records[['sequential_id', 'Name','membership_clusters_ordered']]
+
+#need to save this to the database
+best_cluster_users = get_best_cluster_users(best_cluster_data, 'membership_clusters_ordered', 10)
--- src/notification_system/notification.py
+++ src/notification_system/notification.py
@@ -8,7 +8,7 @@ from dotenv import find_dotenv, load_dotenv
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer, SignatureExpired, BadSignature
 from flask import Flask, jsonify, render_template
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import messagebox, Scrollbar, Canvas, Frame
 
 sys.path.append('c:\\Users\\navi\\Recuriter_selection_system\\src\\evaluation_system')
 from evaluate import get_best_cluster_users
@@ -19,14 +19,14 @@ load_dotenv(find_dotenv())
 # Define email account details
 recruiter_email = os.getenv("DOODLE_MAIL")
 recruiter_password = os.getenv("DOODLE_PASSWORD")
-SECRET_KEY = secrets.token_hex(16)  # Generate a secret key
+SECRET_KEY = secrets.token_hex(16)
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = SECRET_KEY
 
-def generate_secure_link(base_link):
+def generate_secure_link(base_link, user_name):
     s = Serializer(app.config['SECRET_KEY'], expires_in=120)
-    token = s.dumps({'link_valid': True}).decode('utf-8')
+    token = s.dumps({'link_valid': True, 'user_name': user_name}).decode('utf-8')
     return base_link + token
 
 def verify_secure_link(token):
@@ -34,10 +34,10 @@ def verify_secure_link(token):
     try:
         data = s.loads(token)
     except SignatureExpired:
-        return False  # Valid token, but expired
+        return False, None
     except BadSignature:
-        return False  # Invalid token
-    return True
+        return False, None
+    return True, data['user_name']
 
 @app.route('/', methods=['GET'])
 def recruiter_interface():
@@ -50,9 +50,10 @@ def send_emails_route():
 
 @app.route('/challenge/<token>', methods=['GET'])
 def challenge(token):
-    if verify_secure_link(token):
+    is_valid, user_name = verify_secure_link(token)
+    if is_valid:
         root = tk.Tk()
-        app = ChallengeApp(root)
+        app = ChallengeApp(root, user_name)
         root.mainloop()
         return "Challenge completed!", 200
     else:
@@ -68,47 +69,58 @@ def doodle_mail(recruiter_email, recruiter_password, candidate_email, subject, b
         message['From'] = recruiter_email
         message['To'] = candidate_email
         message['Subject'] = subject
-
         message.attach(MIMEText(body, 'plain'))
 
         server.sendmail(recruiter_email, candidate_email, message.as_string())
         server.quit()
-
         return True
     except Exception as e:
         print(f"Failed to send email: {str(e)}")
         return False
 
-def shortlist_mail(candidate_email):
+def shortlist_mail(candidate_name):
     subject = 'Congratulations! You are Shortlisted for Doodle'
-    message = f'Hello,\n\nCongratulations! We are happy to inform that you have been shortlisted for a coding challenge at Doodle. Please find the link for the coding challenge below and submit it within the alloted time.'
-    
-    link = generate_secure_link('http://127.0.0.1:5000/challenge/')
-    
-    message += f'\n\nClick the link below to access the coding challenge UI:'
-    message += f'\n\n{link}'
-    
+    message = f'Hello,\n\nCongratulations! We are happy to inform that you have been shortlisted for a coding challenge at Doodle. Please find the link for the coding challenge below and submit it within the allotted time.'
+    link = generate_secure_link('http://127.0.0.1:5000/challenge/', candidate_name)
+    message += f'\n\nClick the link below to access the coding challenge UI:\n{link}'
+    candidate_email = "doodlepython8+" + candidate_name.replace(" ", "") + "@gmail.com"
     return doodle_mail(recruiter_email, recruiter_password, candidate_email, subject, message)
 
 def send_emails_to_top_users():
-    best_users = get_best_cluster_users().head(10)
-
+    best_users = get_best_cluster_users().head(30)
     for _, user in best_users.iterrows():
-        candidate_email = "doodlepython8+" + user['Name'].replace(" ", "") + "@gmail.com"
-        shortlisting_success = shortlist_mail(candidate_email)
-
+        shortlisting_success = shortlist_mail(user['Name'])
         if shortlisting_success:
             print(f"Shortlisting email sent successfully to {user['Name']}.")
         else:
             print(f"Failed to send shortlisting email to {user['Name']}.")
 
+def send_selection_mail(candidate_email):
+    subject = 'Congratulations! You passed the challenge!'
+    body = f'Hello,\n\nWe are pleased to inform you that you have successfully passed our coding challenge. We were impressed with your solutions and would like to proceed to the next steps of the hiring process.\n\nPlease await further instructions from our recruitment team.'
+    return doodle_mail(recruiter_email, recruiter_password, candidate_email, subject, body)
+
+def send_rejection_mail(candidate_email):
+    subject = 'Thank you for participating'
+    body = f'Hello,\n\nThank you for taking the time to participate in our coding challenge. Unfortunately, based on the answers provided, we will not be proceeding to the next steps of the hiring process at this time.\n\nWe wish you all the best in your future endeavors.'
+    return doodle_mail(recruiter_email, recruiter_password, candidate_email, subject, body)
+
 class ChallengeApp:
-    def __init__(self, root):
+    def __init__(self, root, user_name):
         self.root = root
         self.root.title("Doodle Challenge")
+        self.user_name = user_name
+
+        self.canvas = Canvas(self.root)
+        self.canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+        self.scrollbar = Scrollbar(self.root, command=self.canvas.yview)
+        self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+        self.canvas.configure(yscrollcommand=self.scrollbar.set)
+        self.frame = Frame(self.canvas)
+        self.canvas.create_window((0, 0), window=self.frame, anchor='nw')
 
         self.time_left = 120
-        self.timer_label = tk.Label(root, text=f"Time left: {self.time_left} seconds")
+        self.timer_label = tk.Label(self.frame, text=f"Time left: {self.time_left} seconds")
         self.timer_label.pack(pady=10)
 
         self.questions = [
@@ -120,16 +132,18 @@ class ChallengeApp:
         self.answer_fields = []
 
         for q in self.questions:
-            q_label = tk.Label(root, text=q, wraplength=500)
+            q_label = tk.Label(self.frame, text=q, wraplength=500)
             q_label.pack(pady=10)
-            answer_field = tk.Text(root, height=10, width=50)
+            answer_field = tk.Text(self.frame, height=10, width=50)
             answer_field.pack(pady=10)
             self.answer_fields.append(answer_field)
 
-        self.submit_button = tk.Button(root, text="Submit", command=self.submit_response)
+        self.submit_button = tk.Button(self.frame, text="Submit", command=self.submit_response)
         self.submit_button.pack(pady=10)
 
         self.update_timer()
+        self.frame.update_idletasks()
+        self.canvas.config(scrollregion=self.canvas.bbox('all'))
 
     def update_timer(self):
         if self.time_left > 0:
@@ -146,6 +160,14 @@ class ChallengeApp:
         print("User's solutions:")
         for idx, solution in enumerate(user_solutions, 1):
             print(f"Question {idx}: {solution}")
+
+        answered_questions = sum(1 for answer in user_solutions if answer)
+        candidate_email = "doodlepython8+" + self.user_name.replace(" ", "") + "@gmail.com"
+
+        if answered_questions > 2:
+            send_selection_mail(candidate_email)
+        else:
+            send_rejection_mail(candidate_email)
 
         self.root.quit()
 
